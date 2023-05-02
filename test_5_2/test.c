0540ab13d2cc2f77f013ec1ae8fd0a500fb0d2b2
TianJiaQi-Code <108409193+TianJiaQi-Code@users.noreply.github.com>
2023-05-02 22:43:04 +0800

    Add files via upload

---
+++ test_5_2/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    char c = getchar();
+//    printf("%d", c);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int hour, minute, k;
+//    scanf("%d:%d %d", &hour, &minute, &k);
+//    minute += k;
+//    hour += minute / 60;
+//    minute %= 60;
+//    while (hour >= 24)
+//    {
+//        hour -= 24;
+//    }
+//    printf("%02d:%02d", hour, minute);
+//    return 0;
+//}
