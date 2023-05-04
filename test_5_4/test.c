bd99fa9e165a230cae1e30fa78b517884fb99422
TianJiaQi-Code <108409193+TianJiaQi-Code@users.noreply.github.com>
2023-05-04 20:28:35 +0800

    Add files via upload

---
+++ test_5_4/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World!\n");
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	printf("%d\n", sizeof(long double));
+}
