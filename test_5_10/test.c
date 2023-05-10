bbffce4377e982ec8859bc2451ea34af95bdbb55
TianJiaQi-Code <108409193+TianJiaQi-Code@users.noreply.github.com>
2023-05-10 22:51:59 +0800

    Add files via upload

---
+++ test_5_10/test.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int NumberOf1(int n)
+//{
+//    int i, count = 0, temp = 1;
+//    while (temp != 0)
+//    {
+//        if ((n & temp) != 0)
+//        {
+//            count++;
+//        }
+//        temp = temp << 1;
+//    }
+//    return count;
+//}
+
+//int main()
+//{
+//	int a = 3, b = 5;
+//	a = a ^ b;
+//	printf("%d %d\n", a, b);
+//	b = a ^ b;
+//	printf("%d %d\n", a, b);
+//	a = a ^ b;
+//	printf("%d %d\n", a, b);
+//	return 0;
+//}
