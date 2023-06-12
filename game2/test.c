156f70dd3b08750cc86ff8b7b29524c88ce2ea16
TianJiaQi <2780888910@qq.com>
2023-06-12 23:08:20 +0800

    扫雷更新

--- game2/test.c
+++ game2/test.c
@@ -2,7 +2,21 @@
 
 #include <stdio.h>
 
+void menu()
+{
+	printf("*****************************\n");
+	printf("*********  0. exit  *********\n");
+	printf("*********  1. play  *********\n");
+	printf("*****************************\n");
+}
+
 int main()
 {
+	int input;
+	do
+	{
+		menu();
+		scanf("%d", &input);
+	} while (input);
 	return 0;
 }
