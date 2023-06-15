47c0c9a73620bbd72cc38c5ca33211cd41879995
TianJiaQi <2780888910@qq.com>
2023-06-15 12:51:53 +0800

    扫雷更新

--- game2/test.c
+++ game2/test.c
@@ -5,9 +5,14 @@
 void menu()
 {
 	printf("*****************************\n");
-	printf("*********  0. exit  *********\n");
 	printf("*********  1. play  *********\n");
+	printf("*********  0. exit  *********\n");
 	printf("*****************************\n");
+}
+
+void game()
+{
+
 }
 
 int main()
@@ -16,7 +21,20 @@ int main()
 	do
 	{
 		menu();
+		printf("请选择:");
 		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请重新选择\n");
+			break;
+		}
 	} while (input);
 	return 0;
 }
