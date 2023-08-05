3288e3344a519abb4f682c4953b1cf7391999074
TianJiaQi <2780888910@qq.com>
2023-08-05 22:27:10 +0800

    数据结构：栈和队列

---
+++ Stack/stack.c
@@ -0,0 +1,70 @@
+#include "stack.h"
+
+//初始化
+void STInit(ST* ps)
+{
+	assert(ps);
+	ps->a = NULL;
+	ps->capacity = 0;
+	ps->top = 0;
+}
+
+//销毁
+void STDestroy(ST* ps)
+{
+	assert(ps);
+	free(ps->a);
+	ps->a = NULL;
+	ps->capacity = 0;
+	ps->top = 0;
+}
+
+//插入
+void STPush(ST* ps, STDataType x)
+{
+	assert(ps);
+	if (ps->top == ps->capacity)
+	{
+		int newCapacity = ps->capacity == 0 ? 4 : ps->capacity * 2;
+		STDataType* tmp = (STDataType*)realloc(ps->a, sizeof(STDataType) * newCapacity);
+		if (tmp == NULL)
+		{
+			perror("realloc fail");
+			exit(-1);
+		}
+		ps->a = tmp;
+		ps->capacity = newCapacity;
+	}
+	ps->a[ps->top] = x;
+	ps->top++;
+}
+
+//删除
+void STPop(ST* ps)
+{
+	assert(ps);
+	assert(ps->top > 0);
+	ps->top--;
+}
+
+//返回栈顶元素
+STDataType STTop(ST* ps)
+{
+	assert(ps);
+	assert(ps->top > 0);
+	return ps->a[ps->top - 1];
+}
+
+//计算栈的元素个数
+int STSize(ST* ps)
+{
+	assert(ps);
+	return ps->top;
+}
+
+//判断栈是否为空
+bool STEmpty(ST* ps)
+{
+	assert(ps);
+	return ps->top == 0;
+}
