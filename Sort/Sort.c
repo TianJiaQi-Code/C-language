e229676a00b49c47587541011642427b212f4c87
TianJiaQi <2780888910@qq.com>
2023-09-24 21:27:32 +0800

    排序

--- Sort/Sort.c
+++ Sort/Sort.c
@@ -64,3 +64,210 @@ void ShellSort(int* a, int n)
 		}
 	}
 }
+
+void BubbleSort(int* a, int n)
+{
+	for (int j = 0; j < n; j++)
+	{
+		int exchange = 0;
+		for (int i = 1; i < n - j; i++)
+		{
+			if (a[i - 1] > a[i])
+			{
+				Swap(&a[i - 1], &a[i]);
+				exchange = 1;
+			}
+		}
+		if (exchange == 0)
+			break;
+	}
+}
+
+void AdjustDown(int* a, int n, int parent)
+{
+	int child = parent * 2 + 1;
+	while (child < n)
+	{
+		// 找出小的那个孩子
+		if (child + 1 < n && a[child + 1] > a[child])
+		{
+			++child;
+		}
+		if (a[child] > a[parent])
+		{
+			Swap(&a[child], &a[parent]);
+			// 继续往下调整
+			parent = child;
+			child = parent * 2 + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void HeapSort(int* a, int n)
+{
+	// 向下调整建堆
+	for (int i = (n - 1 - 1) / 2; i >= 0; i--)
+	{
+		AdjustDown(a, n, i);
+	}
+	int end = n - 1;
+	while (end > 0)
+	{
+		Swap(&a[0], &a[end]);
+		AdjustDown(a, end, 0);
+		--end;
+	}
+}
+
+void SelectSort(int* a, int n)
+{
+	int begin = 0, end = n - 1;
+	while (begin < end)
+	{
+		int mini = begin, maxi = begin;
+		for (int i = begin + 1; i <= end; i++)
+		{
+			if (a[i] > a[maxi])
+			{
+				maxi = i;
+			}
+			if (a[i] < a[mini])
+			{
+				mini = i;
+			}
+		}
+		Swap(&a[begin], &a[mini]);
+		// max如果被换走了，修正一下
+		if (maxi == begin)
+		{
+			maxi = mini;
+		}
+		Swap(&a[end], &a[maxi]);
+		++begin;
+		--end;
+	}
+}
+
+// 三数取中
+int GetMidi(int* a, int left, int right)
+{
+	int mid = (left + right) / 2;
+	if (a[left] < a[mid])
+	{
+		if (a[mid] < a[right])
+		{
+			return mid;
+		}
+		else if (a[left] > a[right])	// mid是最大值
+		{
+			return left;
+		}
+		else
+		{
+			return right;
+		}
+	}
+	else    // a[left] > a[mid]
+	{
+		if (a[mid] > a[right])
+		{
+			return mid;
+		}
+		else if (a[left] < a[right])	// mid是最小值
+		{
+			return left;
+		}
+		else
+		{
+			return right;
+		}
+	}
+}
+
+// 快速排序Hoare版本
+int PartSort1(int* a, int left, int right)
+{
+	int midi = GetMidi(a, left, right);
+	Swap(&a[left], &a[midi]);
+	int keyi = left;
+	while (left < right)
+	{
+		// 赵小
+		while (left < right && a[right] >= a[keyi])
+		{
+			--right;
+		}
+		// 找大
+		while (left < right && a[left] <= a[keyi])
+		{
+			++left;
+		}
+		Swap(&a[left], &a[right]);
+	}
+	Swap(&a[keyi], &a[left]);
+	return left;
+}
+
+// 快速排序挖坑法
+int PartSort2(int* a, int left, int right)
+{
+	int midi = GetMidi(a, left, right);
+	Swap(&a[left], &a[midi]);
+	int key = a[left];
+	// 保存key的值后，左边形成第一个坑
+	int hole = left;
+	while (left < right)
+	{
+		// 右边先走，找小，填到左边的坑，右边形成新的坑位
+		while (left < right && a[right] >= key)
+		{
+			--right;
+		}
+		a[hole] = a[right];
+		hole = right;
+		// 左边再走，找大，填到右边的坑，左边形成新的坑位
+		while (left < right && a[left] <= key)
+		{
+			++left;
+		}
+		a[hole] = a[left];
+		hole = left;
+	}
+	a[hole] = key;
+	return hole;
+}
+
+// 快速排序前后指针法
+int PartSort3(int* a, int left, int right)
+{
+	int midi = GetMidi(a, left, right);
+	Swap(&a[left], &a[midi]);
+	int prev = left;
+	int cur = prev + 1;
+	int keyi = left;
+	while (cur <= right)
+	{
+		if (a[cur] < a[keyi] && ++prev != cur)
+		{
+			Swap(&a[prev], &a[cur]);
+		}
+		++cur;
+	}
+	Swap(&a[prev], &a[keyi]);
+	return prev;
+}
+
+void QuickSort(int* a, int begin, int end)
+{
+	if (begin >= end)
+		return;
+	//int keyi = PartSort1(a, begin, end);
+	//int keyi = PartSort2(a, begin, end);
+	int keyi = PartSort3(a, begin, end);
+	QuickSort(a, begin, keyi - 1);
+	QuickSort(a, keyi + 1, end);
+}
--- Sort/Test.c
+++ Sort/Test.c
@@ -1,21 +1,112 @@
+#include <time.h>
+#include <stdlib.h>
 #include "Sort.h"
 
-void InsterTest(int arr[], int n)
+void TestInsertSort()
 {
-	InsertSort(arr, n);
-	PrintArray(arr, n);
+	int arr[10] = { 9,1,3,5,2,7,8,0,6,4 };
+	InsertSort(arr, sizeof(arr) / sizeof(arr[0]));
+	PrintArray(arr, sizeof(arr) / sizeof(arr[0]));
 }
 
-void ShellTest(int arr[], int n)
+void TestShellSort()
 {
-	ShellSort(arr, n);
-	PrintArray(arr, n);
+	int arr[10] = { 9,1,3,5,2,7,8,0,6,4 };
+	ShellSort(arr, sizeof(arr) / sizeof(arr[0]));
+	PrintArray(arr, sizeof(arr) / sizeof(arr[0]));
+}
+
+void TestBubbleSort()
+{
+	int arr[10] = { 9,1,3,5,2,7,8,0,6,4 };
+	BubbleSort(arr, sizeof(arr) / sizeof(arr[0]));
+	PrintArray(arr, sizeof(arr) / sizeof(arr[0]));
+}
+
+void TestHeapSort()
+{
+	int arr[10] = { 9,1,3,5,2,7,8,0,6,4 };
+	HeapSort(arr, sizeof(arr) / sizeof(arr[0]));
+	PrintArray(arr, sizeof(arr) / sizeof(arr[0]));
+}
+
+void TestSelectSort()
+{
+	int arr[10] = { 9,1,3,5,2,7,8,0,6,4 };
+	SelectSort(arr, sizeof(arr) / sizeof(arr[0]));
+	PrintArray(arr, sizeof(arr) / sizeof(arr[0]));
+}
+
+void TestQuickSort()
+{
+	int arr[10] = { 9,1,3,5,2,7,8,0,6,4 };
+	QuickSort(arr, 0, sizeof(arr) / sizeof(arr[0]) - 1);
+	PrintArray(arr, sizeof(arr) / sizeof(arr[0]));
+}
+
+void TestOP()
+{
+	srand((unsigned int)time(0));
+	const int N = 100000;
+	int* a1 = (int*)malloc(sizeof(int) * N);
+	int* a2 = (int*)malloc(sizeof(int) * N);
+	int* a3 = (int*)malloc(sizeof(int) * N);
+	int* a4 = (int*)malloc(sizeof(int) * N);
+	int* a5 = (int*)malloc(sizeof(int) * N);
+	int* a6 = (int*)malloc(sizeof(int) * N);
+
+	for (int i = 0; i < N; i++)
+	{
+		a1[i] = rand();
+		a2[i] = a1[i];
+		a3[i] = a1[i];
+		a4[i] = a1[i];
+		a5[i] = a1[i];
+		a6[i] = a1[i];
+	}
+
+	int begin1 = clock();
+	InsertSort(a1, N);
+	int end1 = clock();
+
+	int begin2 = clock();
+	ShellSort(a2, N);
+	int end2 = clock();
+
+	int begin3 = clock();
+	SelectSort(a3, N);
+	int end3 = clock();
+
+	int begin4 = clock();
+	HeapSort(a4, N);
+	int end4 = clock();
+
+	int begin5 = clock();
+	QuickSort(a5, 0, N - 1);
+	int end5 = clock();
+
+	printf("InsertSort:%d\n", end1 - begin1);
+	printf("ShellSort:%d\n", end2 - begin2);
+	printf("SelectSort:%d\n", end3 - begin3);
+	printf("HeapSort:%d\n", end4 - begin4);
+	printf("QuickSort:%d\n", end5 - begin5);
+
+	free(a1);
+	free(a2);
+	free(a3);
+	free(a4);
+	free(a5);
+	free(a6);
 }
 
 int main()
 {
-	int arr[10] = { 9,1,3,5,2,7,8,0,6,4 };
-	//InsterTest(arr, sizeof(arr) / sizeof(arr[0]));
-	ShellTest(arr, sizeof(arr) / sizeof(arr[0]));
+	//TestInsertSort();
+	//TestShellSort();
+	//TestBubbleSort();
+	//TestHeapSort();
+	//TestSelectSort();
+	//TestQuickSort();
+	TestOP();
 	return 0;
 }
