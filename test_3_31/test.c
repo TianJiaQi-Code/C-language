ab218a1b3bc2d1591030bf99b93d5d3cd797ba08
TianJiaQi <2780888910@qq.com>
2024-03-31 21:17:50 +0800

    2024真题

---
+++ test_3_31/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//void func(int x)
+//{
+//    printf("%d", x);
+//}
+
+int main()
+{
+    int a = 5 / 3;
+    int b = 5.0 / 3;
+
+    printf("%d,%d", a, b);
+    return 0;
+}
